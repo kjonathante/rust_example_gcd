e030122093be13e858be17c738200986cf715a01
Kit Jonathan Te <kjonathante@gmail.com>
2022-02-21 17:51:06 -0800

    Added the gcd function

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,16 @@ fn main() {
         println!("{}", arg);
     }
 }
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
